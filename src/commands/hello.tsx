ca16b8e894729963b16c2b9386a3f7426cc3b2e4
Jake <dawkinsjh@gmail.com>
2020-01-23 13:30:26 -0500

    working transport

--- src/commands/hello.tsx
+++ src/commands/hello.tsx
@@ -1,7 +1,7 @@
 import { Command, flags } from "@oclif/command";
 import winston from "winston";
 import Transport from "winston-transport";
-import React, { Component } from "react";
+import React from "react";
 import { render, Color } from "ink";
 
 class WinstonInkTransport extends Transport {
@@ -9,16 +9,13 @@ class WinstonInkTransport extends Transport {
     super(opts);
   }
 
-  log({ level, message }: any) {
-    render(
-      <Color
-        red={level === "error"}
-        yellow={level === "warn"}
-        cyan={level === "info"}
-      >
-        {level}: {message}
+  log({ level, message }: any, cb: any) {
+    const { waitUntilExit } = render(
+      <Color key={message} red={level === "error"} yellow={level === "warn"}>
+        [{level}] {message}
       </Color>
     );
+    waitUntilExit().then(cb);
   }
 }
 
@@ -27,30 +24,23 @@ const logger = winston.createLogger({
     // new winston.transports.Console({
     //   format: winston.format.json()
     // }),
-    new WinstonInkTransport({ level: "info" }),
-    new WinstonInkTransport({ level: "warn" })
-  ]
+    new WinstonInkTransport({})
+  ],
+  exitOnError: false
 });
 
 export default class Hello extends Command {
-  static description = "describe the command here";
-
   static flags = {
-    help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
     name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
     force: flags.boolean({ char: "f" })
   };
 
   async run() {
-    const { args, flags } = this.parse(Hello);
-
+    const { flags } = this.parse(Hello);
     const name = flags.name || "world";
+
     logger.info(`hello ${name} from ./src/commands/hello.ts`);
 
-    if (flags.force) {
-      logger.warn(`you input --force`);
-    }
+    if (flags.force) logger.warn(`The --force is with you, ${name}.`);
   }
 }
